7014db1b497e253fd53a235a4eea4baccebcefc6
qbentil <Bentilshadrack72@gmail.com>
2022-01-20 13:23:27 +0000

    Normalized Digits Component

---
+++ utils/normalizeDigits.js
@@ -0,0 +1,11 @@
+const normalizeDigits = value => {
+    if(value.toString().length < 2){
+      return '0' + value
+    }else if(value.toString().length < 1 || value.toString() == '0' || Number(value) == 0
+    ){
+        return "00"
+    }
+    return value;
+}
+
+export default normalizeDigits;
