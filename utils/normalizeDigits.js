4fd60a09157677af91498a308964ae1ef1294b7b
qbentil <Bentilshadrack72@gmail.com>
2022-01-24 09:52:20 +0000

    Plete normalizedDigits component

--- utils/normalizeDigits.js
+++ utils/normalizeDigits.js
@@ -1,10 +1,9 @@
 const normalizeDigits = value => {
-    // if(value.toString().length < 2){
-    //   return '0' + value
-    // }else if(value.toString().length < 1 || value.toString() == '0' || Number(value) == 0
-    // ){
-    //     return "00"
-    // }
+    if(value.toString().length < 2){
+      return '0' + value;
+    }else if(value.toString().length < 1 || value.toString() == '0' || Number(value) == 0){
+        return "00";
+    }
     return value;
 }
 
